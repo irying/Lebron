be5a4eea26de96574419f22f99252a5be27bdd78
irying <kyrieup@gmail.cn>
2017-04-04 17:13:45 +0800

    add map2

--- laravel-collection/map2.php
+++ laravel-collection/map2.php
@@ -5,3 +5,23 @@
  * @author kyrie
  * @date: 2017/4/4 下午4:58
  */
+require __DIR__ . '/vendor/autoload.php';
+$gates = [
+    'cool_one_A1',
+    'cool_Two_A2',
+    'cool_A3',
+    'B1',
+];
+
+// common way
+$board = collect($gates)->map(function ($gate){
+    $parts = explode('_', $gate);
+    return end($parts);
+});
+
+// better way
+$boardToo = collect($gates)->map(function ($gate){
+    return collect(explode('_', $gate))->last();
+});
+dump($board, $boardToo);
+
